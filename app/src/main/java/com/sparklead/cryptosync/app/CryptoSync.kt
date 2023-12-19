f91a87006e5ec5729ed668a977e80e86468def53
Aditya Gupta <ashishgupta9936ad@gmail.com>
2023-12-19 11:30:13 +0530

    app: di setup

--- app/src/main/java/com/sparklead/cryptosync/app/CryptoSync.kt
+++ app/src/main/java/com/sparklead/cryptosync/app/CryptoSync.kt
@@ -1,4 +1,7 @@
 package com.sparklead.cryptosync.app
 
-class CryptoSync {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoSync : Application()
